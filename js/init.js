1ddd0ea71bb8dcf9d0bca4016d9b3289a41ba7d6
Alvaro Abarca <abargdev@gmail.com>
2023-12-11 20:10:26 -0600

    chat

---
+++ js/init.js
@@ -0,0 +1,11 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAjS-pf1JC5y73eKDJhhhwpZYm86lGHmEc",
+  authDomain: "ejemplo-chat-5c06e.firebaseapp.com",
+  projectId: "ejemplo-chat-5c06e",
+  storageBucket: "ejemplo-chat-5c06e.appspot.com",
+  messagingSenderId: "1048955728767",
+  appId: "1:1048955728767:web:1986d44bab945c9d7aa04a"
+    });
